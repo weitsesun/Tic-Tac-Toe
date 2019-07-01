f1029b0d8854ff4694cdb8f88e0034cc62122ecb
Wayne Sun <wtse.sun@gmail.com>
2019-07-01 16:31:24 -0700

    implemented webpack

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,5 @@
-import React from 'React';
-import ReactDOM from 'ReactDOM';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,16 @@
-const path = require('path');
 module.exports = {
-  mode: 'development',
-  entry: path.join(__dirname, 'client/index.jsx'),
-  watch: true,
-  output: {
-    path: __dirname + 'dist',
-    publicPath: 'client/dist/',
-    filename: "bundle.js",
-  },
+  entry: './client/index.jsx',
+  output: {path: __dirname + '/client/dist/',
+          filename: 'bundle.js'},
   module: {
-    rules: [{
-      test: /.jsx?$/,
-      include: [
-        path.resolve(__dirname, 'client/app')
-      ],
-      exclude: [
-        path.resolve(__dirname, 'node_modules')
-      ],
-      use: ['babel-loader'],
-    }]
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
   }
 };
